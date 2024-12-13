a0cfb9b6846f7a531eadff57e9c024c938d470ca
aparnabhave28 <aparnabhave28@gmail.com>
2024-12-13 20:04:58 +0530

    Changes made iN bROWSER Class as per comment

--- src/main/java/pojo/Browser.java
+++ src/main/java/pojo/Browser.java
@@ -1,6 +1,9 @@
 package pojo;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -12,10 +15,10 @@ public class Browser {
 	public static WebDriver launchbrowser(String name) {
 		if(name.equals("Chrome")) 
 		{
-		WebDriverManager.chromedriver().setup();// browser setup
+		WebDriverManager.chromedriver().setup();
 		ChromeOptions opt= new ChromeOptions();
 		opt.addArguments("--remote-allow-origins=*");
-	    driver=new ChromeDriver(opt);//casting
+	    driver=new ChromeDriver(opt);
 		}
 		else if(name.equals("Edge")) {
 			WebDriverManager.edgedriver().setup();
@@ -28,6 +31,7 @@ public class Browser {
 	
 		driver.get("https://www.naaptol.com");
 		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		return driver;
 		
 	}
